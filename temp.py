cc155152aecf2d05d892096b806b99288747321b
Gerardo Marx <gmarx_cc@itmorelia.edu.mx>
2023-09-12 11:28:55 +0200

    Python module for Raspberry

--- temp.py
+++ temp.py
@@ -2,7 +2,16 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp", "r")
-temp = fo.read()
-print(temp)
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    temp = fo.read()
+    return int(temp)
+
+def averageTemp(n):
+    sum=0
+    for i in range(n):
+        sum = sum + readTemp()
+    average = sum/n
+    return average
+
 
